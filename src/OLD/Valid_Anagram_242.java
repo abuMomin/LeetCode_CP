414412a2cbb193971e55e7117b5c404b7d6e6f73
AbuMomin <amomin193076@bscse.uiu.ac.bd>
2025-02-06 19:15:07 +0600

    Initial push

---
+++ src/OLD/Valid_Anagram_242.java
@@ -0,0 +1,61 @@
+package OLD;
+
+import java.util.Scanner;
+
+// Can be solved using array --> Runtime: O(n2)
+// Can be solved using Arraylist --> Runtime: O(n2)
+// optimally solution is using HashMap --> Runtime; O(n)
+public class Valid_Anagram_242 {
+    /*
+     //using ArrayList is bad idea
+     public boolean isAnagram(String s, String t) {
+         ArrayList<Character> arrayList = new ArrayList<Character>();
+
+         for (int i = 0;  i < s.length(); i++){
+             arrayList.add((Character) s.charAt(i));
+         }
+
+         for (int j = 0; j < t.length(); j++){
+             boolean ret = arrayList.remove((Character) t.charAt(j));
+             if (!ret)
+                 return false;
+         }
+
+         if(arrayList.size() == 0)
+             return true;
+         else
+             return false;
+
+     }
+
+     */
+
+    //let's try with array
+    public boolean isAnagram(String s, String t){
+        if (s.length() != t.length())
+            return false;
+        int[] arr = new int[26]; // as only small a-z will be INPUT
+
+        for(int i = 0; i < s.length(); i++){
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+        for (int num: arr){
+            if (num != 0)
+                return false;
+        }
+        return true;
+    }
+
+    //Lets try with hashmap
+//    public boolean isAnagram(String s, String t) {
+//
+//    }
+
+    public static void main(String[] args) {
+        String str1 = new Scanner(System.in).next();
+        String str2 = new Scanner(System.in).next();
+
+        System.out.println(new Valid_Anagram_242().isAnagram(str1, str2));
+    }
+}
---
+++ src/OLD/first_bad_version_278.java
@@ -0,0 +1,64 @@
+package OLD;
+
+import java.util.Scanner;
+
+class VersionControl{
+    boolean[] version;
+    static int calling = 0;
+    boolean isBadVersion(int n){
+        calling++;
+        return version[n];
+    }
+    VersionControl(int size, int badStarts){
+        version = new boolean[size];
+        for (int i = 0; i < size; i++){
+            if(i < badStarts - 1)
+                version[i] = false;
+            else
+                version[i] = true;
+        }
+    }
+}
+
+class Solution extends VersionControl{
+
+    Solution(int size, int badStarts) {
+        super(size, badStarts);
+    }
+    public int firstBadVersion(int n) {
+        //applying binary search to find bad version
+        long start = 0, mid = 0, end = n-1;
+        while (start <= end){
+            mid = (start+end)/2;
+            if (isBadVersion((int)mid) == true){
+                if (mid == 0)
+                    return (int)mid+1;
+                else if (isBadVersion((int)mid - 1) == false)
+                    return (int)mid+1;
+                else
+                    end = mid -1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
+
+public class first_bad_version_278 {
+    public static void main(String[] args) {
+        // For practice --> not related with problem
+        System.out.print("Enter total versions : ");
+        int size = new Scanner(System.in).nextInt();
+        System.out.print("Enter bad version Starts from : ");
+        int badversionStarts = new Scanner(System.in).nextInt();
+        //main codes starts after it.
+
+        Solution solve = new Solution(size, badversionStarts);
+//        for (int i = 0; i < size; i++){
+//            System.out.print(solve.isBadVersion(i) + "\t");
+//        }
+        System.out.println("\nBad Version Starts from --> " + solve.firstBadVersion(size) + "\t Called API --> " + solve.calling);
+    }
+}
